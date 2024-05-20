bc268f1678bc29775a783f5a64c63c59da6c42a3
senior9 <ovi215188@gmail.com>
2024-05-20 07:56:39 +0600

    basic Project setup for all project

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from './app';
+import config from './config';
+
+// getting-started.js
+// const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
+// main().catch(err => console.log(err));
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
