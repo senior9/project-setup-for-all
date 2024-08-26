3496b19a4a42903bbc9fbbaa4710a0f7b8ee2dce
senior9 <ovi215188@gmail.com>
2024-08-27 03:46:59 +0600

    refactor code

--- src/app/modules/student/student.constant.ts
+++ src/app/modules/student/student.constant.ts
@@ -3,3 +3,5 @@ import { TbloodGroup } from "./student.interface";
 export const Bloods: TbloodGroup[] = [
      'A+' , 'A-' , 'B+' , 'B-' , 'AB+' , 'AB-' , 'O+' , 'O-'
   ];
+
+  export const searchAbleFields =['email','name.firstName','presentAdress'];
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -6,6 +6,8 @@ import AppError from "../../errors/AppError";
 import httpStatus from "http-status";
 import { User } from "../user/user.model";
 import { TStudentUpdate } from "./student.interface";
+import QueryBuilder from "../../builder/QueryBuilder";
+import { searchAbleFields } from "./student.constant";
 
 
 // const createStudentDb =async (student:TStudent)=>{
@@ -37,49 +39,82 @@ import { TStudentUpdate } from "./student.interface";
 // }
 
 const getStudentsFromDb = async (query:Record<string,unknown>) => {
-    const searchAbleFields =['email','name.firstName','presentAdress'];
-    let searchTerm= '';
-    const queryObject = {...query} //copy Query 
+    // const searchAbleFields =['email','name.firstName','presentAdress'];
+    // let searchTerm= '';
+    // const queryObject = {...query} //copy Query 
 
-    if(query?.searchTerm){
-        searchTerm=query?.searchTerm as string
-    }
+//     if(query?.searchTerm){
+//         searchTerm=query?.searchTerm as string
+//     }
 
-    // takeout SearchQuery for chaining method for use filtering thats why define search query in different memory
-const searchQuery = Student.find({
-    $or:searchAbleFields.map((field)=>({
-        [field]:{$regex:searchTerm,$options:'i'}
-    }))
-})
+//     // takeout SearchQuery for chaining method for use filtering thats why define search query in different memory
+// const searchQuery = Student.find({
+//     $or:searchAbleFields.map((field)=>({
+//         [field]:{$regex:searchTerm,$options:'i'}
+//     }))
+// })
 
 //filtering
-const excludes = ['searchTerm','sort','limit'];
-excludes.forEach((el)=>delete queryObject[el]);
-console.log(query, queryObject) 
+// const excludeFields = ['searchTerm','sort','limit','page','fields'];
+// excludeFields.forEach((el)=>delete queryObject[el]);
+// console.log(query, queryObject) 
 
-// use query for filtering data
-    const filterQuery = searchQuery.find(queryObject).populate('user').populate({
-        path: 'academicDepartment', /// very very important point 
-        populate: {
-            path: 'academicFaculty'  /// very very important point 
-        }
-    }).populate('admissionSmester');
+// // use query for filtering data
+//     const filterQuery = searchQuery.find(queryObject).populate('user').populate({
+//         path: 'academicDepartment', /// very very important point 
+//         populate: {
+//             path: 'academicFaculty'  /// very very important point 
+//         }
+//     }).populate('admissionSmester');
 
     // sorting method use chaining
-    let sort = '-createdAt';
-    if (query.sort) {
-        sort = query.sort as string;
-    }
-   const sortQuery = filterQuery.sort(sort);
+//     let sort = '-createdAt';
+//     if (query.sort) {
+//         sort = query.sort as string;
+//     }
+//    const sortQuery = filterQuery.sort(sort);
 
-   //limit method use chaining
-    let limit = 1;
-    if (query.limit) {
-        limit = query.limit as number;
-    }
-    const limitQuery = await sortQuery.limit(limit);
+//    //limit method use chaining
+//     let limit = 1;
+//     let page =1;
+//     let skip = 0 ;
+//     if (query.limit) {
+//         limit = query.limit as number;
+//     }
 
-    return limitQuery;
+//     // pagination use chaining 
+//     if (query.page) {
+//         page = query.page as number
+//         skip = (page -1)*limit as number
+//     }
+//     const paginateQuery = sortQuery.skip(skip)
+
+//     const limitQuery =  paginateQuery.limit(limit);
+
+
+    // fields filtering by chaining
+
+    // let fields ='-__v' ;
+    // if (query.fields) {
+    //     fields = (query.fields as string).split(',').join(' ');
+    // }
+
+    // const fieldsFiltering = await limitQuery.select(fields);
+
+    // return fieldsFiltering;
+
+// User QUeryBuilder
+
+const studentQuery = new QueryBuilder(Student.find().populate({
+    path: 'academicDepartment', /// very very important point 
+    populate: {
+        path: 'academicFaculty'  /// very very important point 
+    }
+}).populate('admissionSmester'),query).search(searchAbleFields).filter().sort().paginate().fields();
+
+const result = await studentQuery.modelQuery;
+return result;
+
 }
 
 const getStudentIdFromDb = async (id: string) => {
